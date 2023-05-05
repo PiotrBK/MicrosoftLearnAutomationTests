b1de24cb6e49ee75a88fa728d28e756ed292af1a
PiotrBK <kaczmarek.piotr2@gmail.com>
2023-05-05 11:06:40 +0200

    Commit 2

--- src/main/java/org/testing/automation/homepage/IsAtHomepageTest.java
+++ src/main/java/org/testing/automation/homepage/IsAtHomepageTest.java
@@ -2,13 +2,13 @@ package org.testing.automation.homepage;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.testing.automation.Page;
 import org.testing.automation.pages.HomePage;
+//import org.testing.automation.Page;
 public class IsAtHomepageTest {
 
     @Test
     public void canGoToHomePage(){
-        Page homepage = new HomePage();
+        HomePage homepage = new HomePage();
         homepage.goTo();
         Assertions.assertTrue(homepage.isAt());
 
