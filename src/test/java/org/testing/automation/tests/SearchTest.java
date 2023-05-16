efd3da717aa86efee9fc7bafeae20c20264c4205
PiotrBK <kaczmarek.piotr2@gmail.com>
2023-05-16 12:42:28 +0200

    Commit 2

--- src/test/java/org/testing/automation/tests/SearchTest.java
+++ src/test/java/org/testing/automation/tests/SearchTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.testing.automation.Cleanup;
 import org.testing.automation.Setup;
 import org.testing.automation.pages.HomePage;
+import org.testing.automation.pages.SearchPage;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class SearchTest {
@@ -22,7 +24,10 @@ public class SearchTest {
         HomePage homePage = new HomePage(driver);
         homePage.goTo();
         homePage.searchbox("Azure AI Certification");
-
+        SearchPage searchpage = new SearchPage(driver);
+        searchpage.clickCertyfikaty();
+        searchpage.clickFirstResult();
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://learn.microsoft.com/en-us/certifications/exams/ai-900/"));
     }
 
     @AfterTest
